1103061d1a15da5766936cacb7fd1982d3701b3e
Marshall Danel <marshalldanel@gmail.com>
2019-08-19 23:19:51 -0700

    Add axios to log weather data from search box

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Axios from 'axios';
 import Card from './Card';
 import SearchBox from './SearchBox';
 import './App.css';
 
-function App() {
-  return (
-    <div className='container'>
-      <SearchBox />
-      <Card />
-    </div>
-  );
+class App extends Component {
+  getWeather = (lat, lon) => {
+    const endPoint = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${
+      process.env.REACT_APP_OPEN_WEATHER_API_KEY
+    }`;
+    Axios.get(endPoint)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  render() {
+    return (
+      <div className='container'>
+        <SearchBox getWeather={this.getWeather} />
+        <Card />
+      </div>
+    );
+  }
 }
 
 export default App;
