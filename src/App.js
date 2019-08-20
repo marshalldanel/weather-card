efe26249fc6033a262d3b8ed3a8ff9a33a8c2f80
Marshall Danel <marshalldanel@gmail.com>
2019-08-20 00:30:42 -0700

    Add weather data to state and pass to Card

--- src/App.js
+++ src/App.js
@@ -5,13 +5,31 @@ import SearchBox from './SearchBox';
 import './App.css';
 
 class App extends Component {
+  state = {
+    city: '',
+    temp: '',
+    tempHigh: '',
+    tempLow: '',
+    weather: '',
+    humidity: '',
+    wind: ''
+  };
+
   getWeather = (lat, lon) => {
-    const endPoint = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${
+    const endPoint = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&APPID=${
       process.env.REACT_APP_OPEN_WEATHER_API_KEY
     }`;
     Axios.get(endPoint)
-      .then(res => {
-        console.log(res);
+      .then(({ data }) => {
+        this.setState({
+          city: data.name,
+          temp: data.main.temp,
+          tempHigh: data.main.temp_max,
+          tempLow: data.main.temp_min,
+          weather: data.weather[0].main,
+          humidity: data.main.humidity,
+          wind: data.wind.speed
+        });
       })
       .catch(err => {
         console.log(err);
@@ -21,7 +39,7 @@ class App extends Component {
     return (
       <div className='container'>
         <SearchBox getWeather={this.getWeather} />
-        <Card />
+        <Card {...this.state} />
       </div>
     );
   }
